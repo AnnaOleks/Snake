484ff04282651e2390932242cdcd8b666d7b152a
AnnaO <annaoleks88@gmail.com>
2025-04-15 18:50:03 +0300

    Loeng8 C# (Nasledovanije)

--- Program.cs
+++ Program.cs
@@ -11,14 +11,25 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetWindowSize(80, 25);
+
+            //Отрисовка рамочки
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
+
             Point p1 = new Point(1, 3, '*');
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            HorizontalLine line = new HorizontalLine(5,10,8,'+');
-            line.Draw();
+            
 
             Console.ReadLine();
         }
